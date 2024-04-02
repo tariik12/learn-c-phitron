3359c73b9ad83b4510aa9474f72f7245d652caae
tarikul-islam <tariikul12@gmail.com>
2024-04-02 08:11:10 +0600

    practice problem_1.c solved

---
+++ Module-2.5/prac.tice_problem_1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Recently I heard that you have achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you will shine in your life!\t \t Good luck with all the barriers(^) in your life");
+    return 0; // Added return statement
+}
