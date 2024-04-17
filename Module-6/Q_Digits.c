f82f9a9878622ff76da5bdadd98af5fba457cbb3
tarikul-islam <tariikul12@gmail.com>
2024-04-17 08:13:36 +0600

    nested loop done

--- Module-6/Q_Digits.c
+++ Module-6/Q_Digits.c
@@ -2,13 +2,18 @@
 
 int main()
 {
-   int n;
+   int text;
+   scanf("%d", &text);
+   for (int i=1; i<=text;i++){
+    int n;
    scanf("%d", &n);
    do
    {
-    printf("%d",n%10);
+    printf("%d ",n%10);
     n=n/10;
    } while (n!=0);
    
+   printf("\n");
+   }
     return 0;
 }
---
+++ Module-6/nested_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+   for (int i = 0; i <= 3; i++)
+   {
+    for (int t = 1; t <= 5; t++){
+        printf("%d ", t);
+    }
+    printf("\n");
+    
+    
+   }
+   
+    return 0;
+}
