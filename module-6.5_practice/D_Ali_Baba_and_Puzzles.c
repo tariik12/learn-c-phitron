e02cde40334578c1ec5a902556e2a615b75259ee
tarikul-islam <tariikul12@gmail.com>
2024-04-21 07:52:00 +0600

    6.5 practice problem done

---
+++ module-6.5_practice/D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+   int ar[n];
+   scanf("%d", &n);
+   int a;
+   for(int i=0; i<=n;i++){
+    scanf("%d", &a);
+    for(int t=0; t<a; t++){
+        printf("%d", t);
+    }
+   }
+    return 0;
+}
